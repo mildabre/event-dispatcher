4a85d52d63eda81718c2c6b34b7944a854a73487
milos.brecher <milos.brecher@gmail.com>
2026-02-01 22:59:32 +0100

    feat: EventDispatcherExtension, search listener services by tags, built-in findByTag() works not

--- src/DI/EventDispatcherExtension.php
+++ src/DI/EventDispatcherExtension.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 
 namespace Mildabre\EventDispatcher\DI;
 
-use Mildabre\EventDispatcher\DomainEventListener;
 use Mildabre\EventDispatcher\EventDispatcher;
+use Mildabre\ServiceDiscovery\DI\ServiceDiscoveryExtension;
 use Nette\DI\CompilerExtension;
+use Nette\DI\Definitions\Definition;
 use Nette\Schema\Expect;
 use Nette\Schema\Schema;
 
@@ -26,11 +27,23 @@ class EventDispatcherExtension extends CompilerExtension
         }
 
         $builder = $this->getContainerBuilder();
-        $dispatcher = $builder->addDefinition($this->prefix('dispatcher'))
+        $builder->addDefinition($this->prefix('dispatcher'))
             ->setType(EventDispatcher::class);
 
-        foreach ($builder->findByType(DomainEventListener::class) as $def) {
-            $dispatcher->addSetup('addListener', [$def]);
+    }
+
+    public function beforeCompile(): void
+    {
+        if (!$this->config->enabled) {
+            return;
+        }
+
+        $builder = $this->getContainerBuilder();
+        $dispatcher = $builder->getDefinition($this->prefix('dispatcher'));
+
+        $listeners = array_filter($builder->getDefinitions(), fn(Definition $def) => (bool) $def->getTag(ServiceDiscoveryExtension::TagEventListener));
+        foreach ($listeners as $listener) {
+            $dispatcher->addSetup('addListener', [$listener]);
         }
     }
 }
