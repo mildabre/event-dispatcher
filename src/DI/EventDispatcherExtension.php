64480831c0c27158975b6bdca83c10c91d3757ef
milos.brecher <milos.brecher@gmail.com>
2026-02-16 22:23:57 +0100

    feat: EventDispatcherExtension, implement lazy-create via proxy, move validation logic from EventDispatcher to validateAndExtractEventClass()

--- src/DI/EventDispatcherExtension.php
+++ src/DI/EventDispatcherExtension.php
@@ -4,12 +4,18 @@ declare(strict_types=1);
 
 namespace Mildabre\EventDispatcher\DI;
 
+use LogicException;
+use Mildabre\EventDispatcher\Attributes\Event;
 use Mildabre\EventDispatcher\EventDispatcher;
+use Mildabre\EventDispatcher\ListenerProxy;
+use Mildabre\ServiceDiscovery\Attributes\EventListener;
 use Mildabre\ServiceDiscovery\DI\ServiceDiscoveryExtension;
 use Nette\DI\CompilerExtension;
 use Nette\DI\Definitions\Definition;
 use Nette\Schema\Expect;
 use Nette\Schema\Schema;
+use ReflectionClass;
+use ReflectionNamedType;
 
 class EventDispatcherExtension extends CompilerExtension
 {
@@ -29,7 +35,6 @@ class EventDispatcherExtension extends CompilerExtension
         $builder = $this->getContainerBuilder();
         $builder->addDefinition($this->prefix('dispatcher'))
             ->setType(EventDispatcher::class);
-
     }
 
     public function beforeCompile(): void
@@ -41,9 +46,75 @@ class EventDispatcherExtension extends CompilerExtension
         $builder = $this->getContainerBuilder();
         $dispatcher = $builder->getDefinition($this->prefix('dispatcher'));
 
-        $listeners = array_filter($builder->getDefinitions(), fn(Definition $def) => (bool) $def->getTag(ServiceDiscoveryExtension::TagEventListener));
-        foreach ($listeners as $listener) {
-            $dispatcher->addSetup('addListener', [$listener]);
+        $listenerDefs = array_filter(
+            $builder->getDefinitions(),
+            fn(Definition $def) => (bool) $def->getTag(ServiceDiscoveryExtension::TagEventListener)
+        );
+
+        foreach ($listenerDefs as $listenerName => $listenerDef) {
+            $listenerClass = $listenerDef->getType();
+
+            if (!$listenerClass) {
+                throw new LogicException(sprintf("Listener '%s' must have a type defined.", $listenerName));
+            }
+
+            $eventClass = $this->validateAndExtractEventClass($listenerClass);
+
+            $proxyName = $this->prefix('proxy.' . $listenerName);
+
+            $builder->addDefinition($proxyName)
+                ->setType(ListenerProxy::class)
+                ->setArguments(['serviceName' => $listenerName]);
+
+            $dispatcher->addSetup('addListener', ['@' . $proxyName, $eventClass, $listenerClass]);
         }
+    }
+
+    private function validateAndExtractEventClass(string $class): string
+    {
+        if (!class_exists($class)) {
+            throw new LogicException($class . ", listener class does not exist.");
+        }
+
+        $rc = new ReflectionClass($class);
+
+        if (!$rc->getAttributes(EventListener::class)) {
+            throw new LogicException($class . ", listener must be annotated with #[EventListener] attribute.");
+        }
+
+        if (!$rc->hasMethod('handle')) {
+            throw new LogicException($class . ", listener must implement method handle().");
+        }
+
+        $method = $rc->getMethod('handle');
+        if (!$method->isPublic()) {
+            throw new LogicException($class . ", listener method handle() must be public.");
+        }
+
+        $parameters = $method->getParameters();
+
+        if (count($parameters) !== 1) {
+            throw new LogicException($class . ", listener method handle() must have exactly one parameter, " . count($parameters) . " given.");
+        }
+
+        $parameter = $parameters[0];
+        $type = $parameter->getType();
+
+        if (!$type instanceof ReflectionNamedType || $type->isBuiltin()) {
+            throw new LogicException($class . ", listener method handle() parameter must be a class type (not builtin or union type).");
+        }
+
+        $eventClass = $type->getName();
+
+        if (!class_exists($eventClass)) {
+            throw new LogicException($class . ", listener event class '$eventClass' does not exist.");
+        }
+
+        $eventRc = new ReflectionClass($eventClass);
+        if (!$eventRc->getAttributes(Event::class)) {
+            throw new LogicException($class . ", listener event class '$eventClass' must be annotated with #[Event] attribute.");
+        }
+
+        return $eventClass;
     }
 }
